bc5d2a5ee3883d2b23b16904cc7481e96462f539
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-23 11:44:06 +0100

    Add data to animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,13 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Agumon', '20-2-10', 3, false, 9);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Gabumon', 'Nov 15, 2018', 2, true, 8);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Pikachu', 'Jan 07, 2021', 1, false, 15.04);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Devimon', 'May 12, 2017', 5, true, 11);
